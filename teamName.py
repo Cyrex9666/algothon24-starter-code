e319031f5534acce1682dda652029f221458791e
Isabel Body <isabelbody@gmail.com>
2024-06-24 16:36:28 +1200

    first iteration of linear regression

--- teamName.py
+++ teamName.py
@@ -1,23 +1,41 @@
-
 import numpy as np
-
-##### TODO #########################################
-### RENAME THIS FILE TO YOUR TEAM NAME #############
-### IMPLEMENT 'getMyPosition' FUNCTION #############
-### TO RUN, RUN 'eval.py' ##########################
+from sklearn.linear_model import LinearRegression
 
 nInst = 50
 currentPos = np.zeros(nInst)
-
 
 def getMyPosition(prcSoFar):
     global currentPos
     (nins, nt) = prcSoFar.shape
     if (nt < 2):
         return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
-    lNorm = np.sqrt(lastRet.dot(lastRet))
-    lastRet /= lNorm
-    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
-    currentPos = np.array([int(x) for x in currentPos+rpos])
+    
+    newPos = np.zeros(nins)
+
+    for i in range(nins):
+        prices = prcSoFar[i, :]
+        times = np.arange(nt).reshape(-1, 1)
+        
+        # Fit linear regression model
+        model = LinearRegression().fit(times, prices)
+        trend = model.predict(times)
+        
+        # Calculate standard deviation of residuals
+        residuals = prices - trend
+        std_dev = np.std(residuals)
+        
+        # Calculate upper and lower bounds
+        upper_bound = trend + std_dev
+        lower_bound = trend - std_dev
+        
+        # Determine the position based on crossing the bounds
+        if prices[-1] > upper_bound[-1]:
+            newPos[i] = -5000 / prices[-1]  # Sell
+        elif prices[-1] < lower_bound[-1]:
+            newPos[i] = 5000 / prices[-1]  # Buy
+        else:
+            newPos[i] = 0  # Hold
+
+    currentPos = currentPos + newPos
+    currentPos = np.round(currentPos).astype(int)
     return currentPos
